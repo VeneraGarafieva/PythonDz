f884bc59da762761a1e9139a50f84feb74c073a0
VeneraGarafieva <lbyjcfy@yandex.ru>
2022-05-30 20:08:38 +0500

    23

--- 23/23-02.py
+++ 23/23-02.py
@@ -1,14 +1,23 @@
 from PIL import Image
 
+
 im = Image.open("Рианна.jpeg")
 pixels = im.load()  # список с пикселями
-x, y = im.size  # ширина (x) и высота (y) изображения
-
-for i in range(x):
-    for j in range(y):
+width, height = im.size  # ширина (x) и высота (y) изображения
+list_r = list()
+list_g = list()
+list_b = list()
+for i in range(width):
+    for j in range(height):
         pass
         r, g, b = pixels[i, j]
-
-
+        list_r.append(r)
+        list_g.append(g)
+        list_b.append(b)
+result_r = sum(list_r) // len(list_r)
+result_g = sum(list_g) // len(list_g)
+result_b = sum(list_b) // len(list_b)
+im = Image.new("RGB", (width, height), (result_r, result_g, result_b))
+print(result_r, result_g, result_b)
 im.save("Рианна2.jpg")
 
